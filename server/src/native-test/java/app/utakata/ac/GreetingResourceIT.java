e770b42c6ad54d0a0b1617c5562eb5c82b61bec1
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2026-03-11 11:28:14 +0900

    initial commit

---
+++ server/src/native-test/java/app/utakata/ac/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package app.utakata.ac;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
