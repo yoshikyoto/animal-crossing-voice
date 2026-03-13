56978b8a245da9768cbbd8dd0cafad5d1dea486d
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2026-03-13 15:04:44 +0900

    front を一旦削除

--- front/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-const app = createApp(App)
-app.use(VueAxios, axios)
-app.mount('#app')
